f411f5cd1fc549f851dbc123b8d01c375e64ef78
Iankiburio <ian.kiburio@student.moringaschool.com>
2023-10-10 16:29:41 +0300

    complete

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,29 @@
 def badge_maker(name):
-    return None
+    return "Hello, my name is {}.".format(name)
+
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badges.append(badge_maker(name))
+    return badges
 
-def assign_rooms(names):
-    return None
 
-def printer(names):
-    return None
+def assign_rooms(speakers):
+    room_assignments = []
+    for index, speaker in enumerate(speakers):
+        room_assignments.append("Hello, {}! You'll be assigned to room {}!".format(speaker, index+1))
+    return room_assignments
+
+def printer(speakers):
+    badges = batch_badge_creator(speakers)
+    room_assignments = assign_rooms(speakers)
+
+    for badge in badges:
+        print(badge)
+    
+    for room_assignment in room_assignments:
+        print(room_assignment)
+
+
+printer (["Arel", "Carol"])
